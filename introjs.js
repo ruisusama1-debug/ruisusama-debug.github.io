028aba27d6539eec26a8bd6f8a504c84c0d590f3
ruisusama1-debug <ruisusama1@gmail.com>
2026-09-13 19:55:13 -0600

    Add files via upload

---
+++ introjs.js
@@ -0,0 +1,27 @@
+function abrirMundo(color) {
+
+    const mensajes = {
+
+        rosa:
+        "💗 Bienvenido al Mundo del Amor.\n\n" +
+        "Aquí cada corazón tiene una historia " +
+        "y cada sonrisa ilumina el camino. 🧸💕",
+
+        amarillo:
+        "☀️ Bienvenido al Mundo de la Alegría.\n\n" +
+        "Sonríe, disfruta y deja que la felicidad " +
+        "llene cada momento. 🧸✨",
+
+        morado:
+        "💜 Bienvenido al Mundo de los Sueños.\n\n" +
+        "Aquí la imaginación no tiene límites. " +
+        "¡Atrévete a soñar en grande! ✨🧸",
+
+        azul:
+        "💙 Bienvenido al Mundo de la Amistad.\n\n" +
+        "Un lugar para compartir, disfrutar y " +
+        "crear recuerdos inolvidables. 🧸☁️"
+    };
+
+    alert(mensajes[color]);
+}
